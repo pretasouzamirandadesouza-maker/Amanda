678b707f689000da55ef9a11b20ff65556012345
pretasouzamirandadesouza-maker <seuemail@example.com>
2026-04-16 15:46:32 -0300

    fix android gradle kotlin and download bloc

--- lib/bloc/download/download_bloc.dart
+++ lib/bloc/download/download_bloc.dart
@@ -3,25 +3,21 @@ import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
 class DownloadBloc {
-
-  // 🔥 COLOCA SEU LINK AQUI
-  final String url = "https://www.dropbox.com/scl/fi/w2p0pe346p1o98rrp9bj1/cache.zip?rlkey=scsvr2e3m75ma8jim4wlunkzs&st=fq8dheik&dl=1";
+  final String url = "https://dl.dropboxusercontent.com/scl/fi/w2p0pe346p1o98rrp9bj1/cache.zip?rlkey=scsvr2e3m75ma8jim4wlunkzs";
 
   Future<void> downloadData() async {
     try {
       final dir = await getExternalStorageDirectory();
       final file = File("${dir!.path}/cache.zip");
 
-      print("Baixando data...");
-
       final response = await http.get(Uri.parse(url));
-
       await file.writeAsBytes(response.bodyBytes);
 
-      print("Download concluído: ${file.path}");
-
+      print("Download concluído");
     } catch (e) {
-      print("Erro no download: $e");
+      print("Erro: $e");
     }
   }
+
+  void dispose() {}
 }
